b0e72c3f425971817578ce7b74e88924a52f9f92
mohammad-atique <92262714+mohammad-atique@users.noreply.github.com>
2022-05-09 10:59:13 +0530

    updated schemas

--- src/models/mensWear.models.js
+++ src/models/mensWear.models.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 // mensWear schema
 
 const mensWearSchema = new mongoose.Schema({
+    id:{type: Number, required:true},
     img_url: { type: String, required: true },
     product: { type: String, required: true },
     pro_desc: { type: String, required: true },
--- src/models/womensWear.models.js
+++ src/models/womensWear.models.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const womensSchema = new mongoose.Schema(
   {
+    id:{type: Number, required:true},
     img_url: { type: String, required: true },
     product: { type: String, required: true },
     pro_desc: { type: String, required: true },
